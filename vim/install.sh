ad11edc769dc85427950d23f9d36cae1bba6cdb8
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-11-18 14:26:16 -0600

    vim: Formatting and tiding up.

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-source $DOTFILES/install/spf13-vim-partial
-
 mkdir -p ~/.vim/bundle
 if [ ! -d ~/.vim/bundle/vundle ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
