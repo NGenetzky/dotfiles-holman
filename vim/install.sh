a4a614f3fa0cc425a93d5e078360f1a02781a888
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-11-23 12:16:39 -0600

    vim: vimproc: Add to install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -5,3 +5,8 @@ if [ ! -d ~/.vim/bundle/vundle ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
 fi
 vim +PluginInstall +qall
+
+# vimproc.vim
+if [ -d ~/.vim/bundle/vimproc.vim ]; then
+    ( cd ~/.vim/bundle/vimproc.vim/ && make )
+fi
